85614b8e97bd6c66b34bcd2bfb60e1f6dbcd6713
natxoraga22 <natxoraga22@gmail.com>
2017-04-29 03:30:41 +0200

    Basic menu structure
    SplashScreen
    Start menu
    Options menu
    Win/Lose menus
    Empty level_01 menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelDelay = 0f;
+
+	void Start () {
+		if (autoLoadNextLevelDelay > 0f) Invoke ("LoadNextLevel", autoLoadNextLevelDelay);
+	}
+
+	public void LoadLevel (string levelName) {
+		SceneManager.LoadScene (levelName);
+	}
+
+	public void LoadNextLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame () {
+		Debug.Log ("QuitGame");
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusic;
+
+	private AudioSource audioSource;
+
+
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	private void OnEnable () {
+		SceneManager.sceneLoaded += OnSceneLoaded; // subscribe
+	}
+
+	private void OnDisable () {
+		SceneManager.sceneLoaded -= OnSceneLoaded; //unsubscribe
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode) {
+		audioSource.Stop ();
+		if (levelMusic [scene.buildIndex]) {
+			audioSource.clip = levelMusic [scene.buildIndex];
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+
+}
