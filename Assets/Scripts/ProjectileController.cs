2a6b6943abd2d6837c4d62b648346fb2db518f51
natxoraga22 <natxoraga22@gmail.com>
2017-05-07 23:06:29 +0200

    Defender projectiles animation & movement

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ProjectileController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public float speed;
+	public float damage;
+
+
 	void Update () {
-		
+		this.transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
 }
