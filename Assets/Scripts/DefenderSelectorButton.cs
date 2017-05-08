6c558279a237abf627aee2ba1d35c3b513c3bc52
natxoraga22 <natxoraga22@gmail.com>
2017-05-08 23:38:31 +0200

    Defender selector buttons created

---
+++ Assets/Scripts/DefenderSelectorButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DefenderSelectorButton : MonoBehaviour {
+
+	public GameObject defenderPrefab;
+	public static GameObject selectedDefender;
+
+	private SpriteRenderer spriteRenderer;
+	private DefenderSelectorButton[] buttons;
+
+
+	void Start () {
+		spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
+		buttons = GameObject.FindObjectsOfType<DefenderSelectorButton> ();
+	}
+
+	void OnMouseDown () {
+		// Black out everything
+		foreach (DefenderSelectorButton button in buttons) {
+			button.GetComponentInChildren<SpriteRenderer> ().color = Color.black;
+		}
+		// Set the clicked one to white
+		spriteRenderer.color = Color.white;
+		selectedDefender = defenderPrefab;
+	}
+
+}
