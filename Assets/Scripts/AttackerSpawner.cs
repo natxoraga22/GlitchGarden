04aa182f72cebc3b13653f10a3e9552e09e34d72
natxoraga22 <natxoraga22@gmail.com>
2017-05-10 20:17:38 +0200

    Spawning attackers

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AttackerSpawner : MonoBehaviour {
+
+	public GameObject[] attackerPrefabs;
+	public float[] spawnDelays;
+
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < attackerPrefabs.Length; i++) {
+			if (IsTimeToSpawn (spawnDelays [i])) {
+				Spawn (attackerPrefabs[i]);
+			}
+		}
+	}
+	
+	bool IsTimeToSpawn (float spawnDelay) {
+		// TODO Implement this
+		return true;
+	}
+
+	void Spawn (GameObject attackerPrefab) {
+		int spawnPointIndex = Random.Range (0, this.transform.childCount);
+		Transform spawnPoint = this.transform.GetChild (spawnPointIndex);
+		Instantiate (attackerPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+	}
+
+}
--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Shredder : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D collider) {
-		Destroy (collider.gameObject);
+		if (collider.gameObject.GetComponent<ProjectileController> ()) {
+			Destroy (collider.gameObject);
+		}
 	}
 
 }
